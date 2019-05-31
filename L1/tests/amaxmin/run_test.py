6976473db5494879fe2e8a25d3eb9c76abeaca18
Liang <liangm@xilinx.com>
2019-05-31 08:02:11 -0700

    python scripts to configure and run testbench via profile

--- L1/tests/amaxmin/run_test.py
+++ L1/tests/amaxmin/run_test.py
@@ -1,39 +1,49 @@
 import numpy as np
 import argparse
 import shlex, subprocess
-import os
+import os, sys
+from dataProc import loadProfile
+import json
 
-def main(b_csim, b_synth, b_cosim, p_vectorSize, p_parEntries):
-  a=(np.random.rand(p_vectorSize) - 0.5) * 1e5
-
-  vectorPath = r"./amaxmin/data/vector_%d.csv"%p_vectorSize
-  tclPath = r"./amaxmin/build/run.tcl"
-  np.savetxt(vectorPath, a , fmt='%.8f', delimiter=',')
+def main(testPath, profile):
+  a=(np.random.rand(profile['vectorSize']) - 0.5) * profile['vectorSize']
+  vectorPath = os.path.join(testPath, profile['dataPath'], r"vector_%d.csv"%profile['vectorSize'])
+  if profile['op'] == 'double' or profile['op'] == 'float': 
+    np.savetxt(vectorPath, a , fmt='%.8f', delimiter=',')
+  else:
+    a = a.astype(int) 
+    np.savetxt(vectorPath, a , fmt='%d', delimiter=',')
   a=np.abs(a)
-  amax = np.argmax(a)
-  amin = np.argmin(a)
+  if profile['op'] == 'amax':
+    result = np.argmax(a)
+  elif profile['op'] == 'amin':
+    result = np.argmin(a)
+  else:
+    print('Operation is not supported')
+    sys.exit
 
   commandLine ='''vivado_hls -f %s "runCsim %d runRTLsynth %d \
-runRTLsim %d part vu9p op amin dataType double dataWidth 64 indexType int size %d \
+runRTLsim %d part vu9p op %s dataType %s dataWidth %d indexType int size %d \
 entriesInParallel %d runArgs \
-'%s %d %d'"'''%(tclPath, b_csim, b_synth, b_cosim, p_vectorSize, p_parEntries,
-		   os.path.abspath(vectorPath), p_vectorSize, amin)
-  args = shlex.split(commandLine)
-  subprocess.call(args)
-  
-  commandLine ='''vivado_hls -f %s "runCsim %d runRTLsynth %d \
-runRTLsim %d part vu9p op amax dataType double dataWidth 64 indexType int size %d \
-entriesInParallel %d runArgs \
-'%s %d %d'"'''%(tclPath, b_csim, b_synth, b_cosim, p_vectorSize, p_parEntries,
-		   os.path.abspath(vectorPath), p_vectorSize, amax)
+'%s %d %d'"'''%(os.path.join(testPath, profile['tclPath']), 
+    profile['b_csim'], profile['b_synth'],
+    profile['b_cosim'], profile['op'], profile['dataType'],
+    profile['dataWidth'], profile['vectorSize'], profile['parEntries'],
+    os.path.abspath(vectorPath), profile['vectorSize'], result)
   args = shlex.split(commandLine)
   subprocess.call(args)
   
 if __name__== "__main__":
-  parser = argparse.ArgumentParser(description='Generate random vectors and \
-      run test.')
-  parser.add_argument('size', type=int, metavar='N', help='size of the \
-      vector')
-  parser.add_argument('-p', type=int, help='Number of parallel entries')
+  parser = argparse.ArgumentParser(description='Generate random vectors and run test.')
+  parser.add_argument('-p', type=str, metavar='Profile', help='path to the profile file')
   args = parser.parse_args()
-  main(1, 1, 1 , args.size, args.p)
+  profile=dict()
+  filePath = os.path.realpath(__file__)
+  path = os.path.dirname(filePath)
+  if args.p !=None and os.path.isfile(args.p):
+    profile = loadProfile(args.p)
+    print("profile %s is loaded"%args.p)
+  else:
+    print("profile file is not provided. The default [%s] will be loaded."%(os.path.join(path,'profile.json')))
+    profile = loadProfile(os.path.join(path,'profile.json'))
+  main(path, profile)
