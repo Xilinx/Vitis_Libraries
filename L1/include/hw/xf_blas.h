1ac328588275f6173f8e598a0f9f0bed458f9e87
Liang <liangm@xilinx.com>
2019-06-12 03:14:42 -0700

    add common header file for L1 lib

---
+++ L1/include/hw/xf_blas.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** 
+ * @file xf_blas.h
+ * @brif Top-level header for XF BLAS Library.
+ * 
+ */
+
+#ifndef XF_BLAS_H
+#define XF_BLAS_H
+
+
+// shared modules
+#include "xf_blas/utility.h"
+
+// BLAS L1 function modules
+
+#include "xf_blas/amaxmin.h"
+#include "xf_blas/asum.h"
+#include "xf_blas/axpy.h"
+
+/* TODO
+
+#include "xf_blas/copy.h"
+#include "xf_blas/dot.h"
+#include "xf_blas/scal.h"
+#include "xf_blas/swap.h"
+
+*/
+
+// BLAS L2 function modules
+
+/* TODO
+ *
+#include "xf_blas/gemv.h"
+#include "xf_blas/sbmv.h"
+#include "xf_blas/spmv.h"
+#include "xf_blas/symv.h"
+#include "xf_blas/tpmv.h"
+#include "xf_blas/tbmv.h"
+#include "xf_blas/tbsv.h"
+#include "xf_blas/trmv.h"
+
+*/
+
+// BLAS L3 function modules
+
+/* TODO
+ *
+#include "xf_blas/gemm.h"
+ *
+*/
+
+
+
+#endif
