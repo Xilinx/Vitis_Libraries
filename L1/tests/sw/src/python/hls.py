0767670cdd76e1db1bc7326b954cbf1489cc714b
Liang <liangm@xilinx.com>
2019-06-25 05:57:43 -0700

    resolve license issues

--- L1/tests/sw/src/python/hls.py
+++ L1/tests/sw/src/python/hls.py
@@ -1,4 +1,3 @@
-/*
  # Copyright 2019 Xilinx, Inc.
  #
  # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +12,6 @@
  # See the License for the specific language governing permissions and
  # limitations under the License.
 
-*/
 import shlex, subprocess
 
 class HLS:
