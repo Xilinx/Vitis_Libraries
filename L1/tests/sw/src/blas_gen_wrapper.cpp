fcd7688e0dd339302c47da9d63d63bb5f56d9229
Liang <liangm@xilinx.com>
2019-07-15 01:09:41 -0700

    add BLAS_L2 API

--- L1/tests/sw/src/blas_gen_wrapper.cpp
+++ L1/tests/sw/src/blas_gen_wrapper.cpp
@@ -45,6 +45,24 @@ extern "C" {
       ) {
     return  genBin -> addB1Instr(p_opName, p_n, p_alpha, p_x, p_y, p_xRes, p_yRes, p_res);
   }
+  xfblasStatus_t addB2Instr(GenBinType* genBin,
+      string p_opName,
+      uint32_t p_m,
+      uint32_t p_n,
+      uint32_t p_kl,
+      uint32_t p_ku,
+      BLAS_dataType p_alpha,
+      BLAS_dataType p_beta,
+      void* p_a,
+      void* p_x,
+      void* p_y,
+      void* p_aRes,
+      void* p_yRes
+      ) {
+    return genBin -> addB2Instr(p_opName, p_m, p_n, p_kl, p_ku, 
+        p_alpha, p_beta, 
+        p_a, p_x, p_y, p_aRes, p_yRes);
+  }
   xfblasStatus_t write2BinFile(GenBinType* genBin, const char * p_fileName){
     return  genBin -> write2BinFile(p_fileName);
   }
