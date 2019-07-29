268bd3e00bc7fa7cc031c6a81d6a05ab411250ab
yifei <y.zhou@xilinx.com>
2019-07-29 15:31:07 -0700

    update test

--- L3/tests/gemm/gemm_test.cpp
+++ L3/tests/gemm/gemm_test.cpp
@@ -23,31 +23,20 @@
 #include <string>
 #include <cmath>
 #include <iomanip>
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <assert.h>
+#include <fstream>
+
 #include "xf_blas.hpp"
 
-# define IDX2R(i,j,ld) (((i)*( ld ))+(j))
-# define m 128 // a - mxk matrix
-# define n 128 // b - kxn matrix
-# define k 128 // c - mxn matrix
+#define IDX2R(i, j, ld) (((i) * (ld)) + (j))
 
 using namespace std;
 
-XFBLAS_dataType* getGoldenMat(XFBLAS_dataType* a, XFBLAS_dataType* b, XFBLAS_dataType* c){
-  XFBLAS_dataType * goldenC;
-  goldenC = (XFBLAS_dataType*) malloc(m * n * sizeof (XFBLAS_dataType));
-  for(int row = 0; row < m; row++){ 
-      for(int col = 0; col < n; col++){ 
-          XFBLAS_dataType l_val = 0;
-          for (int i = 0; i < k; i ++) {
-            l_val += a[IDX2R(row,i,k)] * b[IDX2R(i,col,n)];
-          }
-          goldenC[IDX2R(row,col,n)] = l_val + c[IDX2R(row,col,n)];
-      } 
-  }
-  return goldenC;
-}
 
-bool compareGemm(XFBLAS_dataType* c, XFBLAS_dataType* goldenC, float p_TolRel=1e-3, float p_TolAbs=1e-5){
+bool compareGemm(XFBLAS_dataType* c, XFBLAS_dataType* goldenC, int m, int k, int n, float p_TolRel=1e-3, float p_TolAbs=1e-5){
   bool l_check = true;
   for(int row = 0; row < m; row++){ 
     for(int col = 0; col < n; col++){
@@ -72,112 +61,109 @@ int main(int argc, char **argv) {
   
   if (argc < 3){
     cerr << " usage: \n"
-         << " gemx_test.exe gemx.xclbin config_info.dat log.txt 1\n"
-         << " gemx_test.exe gemx.xclbin config_info.dat log.txt\n"
+         << " gemx_test.exe gemx.xclbin config_info.dat m k n\n"
          << " gemx_test.exe gemx.xclbin config_info.dat\n";
     return EXIT_FAILURE; 
   }
-  unsigned int l_argIdx = 1;
-  string l_xclbinFile(argv[l_argIdx++]);
-  string l_configFile(argv[l_argIdx++]);  
+  int l_argIdx = 0;
+  string l_xclbinFile, l_configFile;
+  if (argc > ++l_argIdx) {l_xclbinFile = argv[l_argIdx];cout<<l_xclbinFile<<"\n"; }
+  if (argc > ++l_argIdx) {l_configFile = argv[l_argIdx];}
   string l_logFile;
-  
-  if (argc == 3){
-    ofstream logFile("log.txt");
-    logFile.close();
-    l_logFile = "log.txt";
-  } else {
-    l_logFile = argv[l_argIdx++];
-  }
+
+  ofstream logFile("xrt_report.txt");
+  logFile.close();
+  l_logFile = "xrt_report.txt";
   
   int l_numKernel = 1;
+  int m = 256;
+  int k = 256;
+  int n = 256;
   
-  if (argc == 5){
-    cout<<"read custom number of kernels\n";
-    l_numKernel = stoi(argv[l_argIdx++]); 
-  }
+  if (argc > ++l_argIdx) {m = stoi(argv[l_argIdx]);}
+  if (argc > ++l_argIdx) {k = stoi(argv[l_argIdx]);}
+  if (argc > ++l_argIdx) {n = stoi(argv[l_argIdx]);}
   
-  xfblasEngine_t engineName = XFBLAS_ENGINE_GEMM;
-  xfblasStatus_t status = xfblasCreate(l_xclbinFile.c_str(), l_configFile, l_logFile.c_str(), XFBLAS_ENGINE_GEMM, l_numKernel);
-  if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Create Handle failed with error code: "<< status << "\n"; 
-    xfblasDestory();
-    return EXIT_FAILURE;   
-  }
-    
+  bool test_common;
+  
+  if (argc > ++l_argIdx) {test_common = stoi(argv[l_argIdx]);}
+
+  
   int i, j; // i-row l_numKernel -1 ,j- column l_numKernel -1
-  XFBLAS_dataType * a, * b, * c;
+  XFBLAS_dataType * a, * b, * c, * goldenC;
   
   posix_memalign((void** )&a, 4096, m*k* sizeof ( XFBLAS_dataType ));
   posix_memalign((void** )&b, 4096, k*n* sizeof ( XFBLAS_dataType ));
   posix_memalign((void** )&c, 4096, m*n* sizeof ( XFBLAS_dataType ));
+  posix_memalign((void** )&goldenC, 4096, m*n* sizeof ( XFBLAS_dataType ));
   
-  int ind = 1;
-  for( i = 0; i<  m; i ++){ 
-    for( j = 0; j < k; j ++){ 
-      a[ IDX2R (i,j,k )]= (XFBLAS_dataType) ind++; 
-    } 
-  } 
-  ind = 1;
-  for( i = 0; i<  k; i ++){ 
-    for( j = 0; j < n; j ++){ 
-      b[ IDX2R (i,j,n )]= (XFBLAS_dataType) ind++; 
-    } 
-  } 
+  ifstream inFile;
+  string data_dir("./data/");
 
-  for( i = 0; i<  m; i ++){ 
-    for( j = 0; j < n; j ++){ 
-      c[ IDX2R (i,j,n )]= 0; 
-    } 
-  } 
+  inFile.open(data_dir+"matA_in_"+to_string(m)+"_"+to_string(k)+".bin", ifstream::binary);
+  if( inFile.is_open() ){
+    inFile.read( (char*) a, sizeof(XFBLAS_dataType)*m*k );
+    inFile.close();
+  } else {
+    cerr << "Error opening "<<(data_dir+"matA_in_"+to_string(m)+"_"+to_string(k)+".bin")<<endl;
+    exit(1);
+  }
   
-  XFBLAS_dataType* goldenC = getGoldenMat(a,b,c);
+  inFile.open(data_dir+"matB_in_"+to_string(k)+"_"+to_string(n)+".bin", ifstream::binary);
+  if( inFile.is_open() ){
+    inFile.read( (char*) b, sizeof(XFBLAS_dataType)*k*n );
+    inFile.close();
+  } else {
+    cerr << "Error opening "<<(data_dir+"matB_in_"+to_string(k)+"_"+to_string(n)+".bin")<<endl;
+    exit(1);
+  }
+
+  inFile.open(data_dir+"matC_in_"+to_string(m)+"_"+to_string(n)+".bin", ifstream::binary);
+  if( inFile.is_open() ){
+    inFile.read( (char*) c, sizeof(XFBLAS_dataType)*m*n );
+    inFile.close();
+  } else {
+    cerr << "Error opening "<<(data_dir+"matC_in_"+to_string(m)+"_"+to_string(n)+".bin")<<endl;
+    exit(1);
+  }
   
-  status = xfblasMallocRestricted(m,k,sizeof(*a),a,k, l_numKernel-1);
+  inFile.open(data_dir+"matC_out_"+to_string(m)+"_"+to_string(n)+".bin", ifstream::binary);
+  if( inFile.is_open() ){
+    inFile.read( (char*) goldenC, sizeof(XFBLAS_dataType)*m*n );
+    inFile.close();
+  } else {
+    cerr << "Error opening "<<(data_dir+"matC_out_"+to_string(m)+"_"+to_string(n)+".bin")<<endl;
+    exit(1);
+  }
+  
+  xfblasEngine_t engineName = XFBLAS_ENGINE_GEMM;
+  xfblasStatus_t status = xfblasCreate(l_xclbinFile.c_str(), l_configFile, l_logFile.c_str(), engineName, l_numKernel);
   if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Malloc memory for matrix A failed with error code: "<< status << "\n"; 
-    xfblasDestory();
+    cout<<"Create Handle failed with error code: "<< status << "\n"; 
     return EXIT_FAILURE;   
   }
   
-  status = xfblasMallocRestricted(k,n,sizeof(*b),b,n, l_numKernel-1);
-  
-  if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Malloc memory for matrix B failed with error code: "<< status << "\n"; 
-    xfblasDestory();
-    return EXIT_FAILURE;   
-  }
-  status = xfblasMallocRestricted(m,n,sizeof(*c),c,n, l_numKernel-1);
-  
-  if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Malloc memory for matrix C failed with error code: "<< status << "\n"; 
-    xfblasDestory();
-    return EXIT_FAILURE;   
-  }
-  
-  status = xfblasSetMatrixRestricted(a, l_numKernel-1);
-  status = xfblasSetMatrixRestricted(b, l_numKernel-1);
-  status = xfblasSetMatrixRestricted(c, l_numKernel-1);
-  if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Set Matrix failed with error code: "<< status << "\n"; 
-    xfblasDestory();
-    return EXIT_FAILURE;   
-  }
-  
-  status = xfblasGemm(XFBLAS_OP_N, XFBLAS_OP_N, m, k, n, 1, a, k, b, n, 1, c, n, l_numKernel-1);
-  
-  if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Matrix Multiplication failed with error code: "<< status << "\n"; 
-    xfblasDestory();
-    return EXIT_FAILURE;   
-  }
-  
-  status = xfblasGetMatrixRestricted(c, l_numKernel-1);
-  
-  if (status != XFBLAS_STATUS_SUCCESS) {
-    cout<<"Get Matirx failed with error code: "<< status << "\n"; 
-    xfblasDestory();
-    return EXIT_FAILURE;   
+  if (test_common){
+    cout<<"test default version\n";
+    XFBLAS_dataType * d_a, * d_b, * d_c;
+    status = xfblasMalloc(&d_a, m,k,sizeof(*a), l_numKernel-1);
+    status = xfblasMalloc(&d_b, k,n,sizeof(*b), l_numKernel-1);
+    status = xfblasMalloc(&d_c, m,n,sizeof(*c), l_numKernel-1);
+    status = xfblasSetMatrix(m,k,sizeof(*a),a,k,d_a, l_numKernel-1);
+    status = xfblasSetMatrix(k,n,sizeof(*b),b,n,d_b, l_numKernel-1);
+    status = xfblasSetMatrix(m,n,sizeof(*c),c,n,d_c, l_numKernel-1);
+    status = xfblasGemm(XFBLAS_OP_N, XFBLAS_OP_N, m, k, n, 1, d_a, k, d_b, n, 1, d_c, n, l_numKernel-1);
+    status = xfblasGetMatrix(m,n,sizeof(*c),d_c,c,n, l_numKernel-1);   
+  } else {
+    cout<<"test restricted version\n";
+    status = xfblasMallocRestricted(m,k,sizeof(*a),a,k, l_numKernel-1);
+    status = xfblasMallocRestricted(k,n,sizeof(*b),b,n, l_numKernel-1);
+    status = xfblasMallocRestricted(m,m,sizeof(*c),c,n, l_numKernel-1);
+    status = xfblasSetMatrixRestricted(a, l_numKernel-1);
+    status = xfblasSetMatrixRestricted(b, l_numKernel-1);
+    status = xfblasSetMatrixRestricted(c, l_numKernel-1);
+    status = xfblasGemm(XFBLAS_OP_N, XFBLAS_OP_N, m, k, n, 1, a, k, b, n, 1, c, n, l_numKernel-1);
+    status = xfblasGetMatrixRestricted(c, l_numKernel-1);
   }
   
   for ( i = 0; i < 10; i ++){
@@ -187,7 +173,7 @@ int main(int argc, char **argv) {
     cout<<"\n";
   }
   
-  if (compareGemm(c,goldenC)){
+  if (compareGemm(c,goldenC,m,k,n)){
     cout<<"Test passed!\n";
   }else{
     cout<<"Test failed!\n";
@@ -199,7 +185,7 @@ int main(int argc, char **argv) {
   free(a);
   free(b);
   free(c);
-  
+    
   xfblasDestory(l_numKernel);
 
   return EXIT_SUCCESS;
