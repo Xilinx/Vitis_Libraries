da59cb3bf1db01b25c786e6d553783b7a0193a78
Liang <liangm@xilinx.com>
2019-08-02 09:40:33 -0700

    daily_latest is not available currently, use 0801 instead

--- L1/tests/set_env.sh
+++ L1/tests/set_env.sh
@@ -12,7 +12,7 @@ export PATH=/proj/rdi-xsj/staff/lingl/anaconda2/bin:$PATH
 . /proj/rdi-xsj/staff/lingl/anaconda2/etc/profile.d/conda.sh
 conda activate xf_blas_3_6_7
 
-export TA_PATH=/proj/xbuilds/2019.1_daily_latest/installs/lin64
+export TA_PATH=/proj/xbuilds/2019.1_0801_1400/installs/lin64
 export XILINX_SDX=${TA_PATH}/SDx/2019.1
 export XILINX_VIVADO=${TA_PATH}/Vivado/2019.1
 export LD_LIBRARY_PATH=`$XILINX_SDX/bin/ldlibpath.sh $XILINX_SDX/lib/lnx64.o`:${XILINX_SDX}/lnx64/tools/opencv
