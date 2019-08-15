84ab1f85053bc152479462655b810a9c2f16b21b
Liang <liangm@xilinx.com>
2019-08-15 10:09:50 -0700

    split files

--- L1/tests/sw/python/run_test.py
+++ L1/tests/sw/python/run_test.py
@@ -21,12 +21,11 @@ import json
 import pdb
 import time
 import traceback
-from blas_gen_bin import BLAS_GEN, BLAS_ERROR
-from hls import HLS, HLS_ERROR, Parameters
-from makefile import Makefile
-from operation import OP, BLAS_L1, BLAS_L2, OP_ERROR
+from blas_gen_bin import BLAS_ERROR
+from hls import HLS_ERROR
 import threading
 import concurrent.futures
+from runTest import RunTest
 
 def Format(x):
   f_dic = {0:'th', 1:'st', 2:'nd',3:'rd'}
@@ -34,175 +33,6 @@ def Format(x):
   if k>=4:
     k=0
   return "%d%s"%(x, f_dic[k])
-
-class RunTest:
-  def __init__(self, profile, args, opLocks):
-    self.opLocks = opLocks
-    self.profilePath = profile
-    self.profile = None 
-    self.parEntries = 1
-    self.logParEntries = 0
-    self.valueRange = None
-    self.numToSim = 1
-    self.numSim = 0
-    self.makefile = args.makefile
-
-    self.hls = None
-    self.typeDict ={
-      np.int8: 'int8_t',
-      np.int16: 'int16_t',
-      np.int32: 'int32_t',
-      np.int64: 'int64_t',
-      np.uint8: 'uint8_t',
-      np.uint16: 'uint16_t',
-      np.uint32: 'uint32_t',
-      np.uint64: 'uint64_t',
-      np.float32: 'float',
-      np.float64: 'double'
-    }
-
-  def parseProfile(self):
-    with open(self.profilePath, 'r') as fh:
-      self.profile = json.loads(fh.read())
-
-    self.opName = self.profile['op']
-    self.opClass = OP.parse(self.opName)
-
-    self.minValue = self.profile['valueRange'][0]
-    self.maxValue = self.profile['valueRange'][1]
-
-    self.op = eval(self.opClass).parse(self.opName, self.maxValue, self.minValue)
-
-    if 'logParEntries' in self.profile:
-      self.logParEntries = self.profile['logParEntries']
-    else:
-      self.logParEntries = -1
-
-    if self.logParEntries == -1:
-      self.parEntries = self.profile['parEntries']
-    else:
-      self.parEntries = 1 << self.logParEntries
-
-    if 'dataTypes' in self.profile:
-      self.dataTypes = [eval('np.%s'%dt) for dt in self.profile['dataTypes']]
-
-    if 'retTypes' in self.profile:
-      self.retTypes = [eval('np.%s'%dt) for dt in self.profile['retTypes']]
-    
-    self.numToSim = self.profile['numSimulation']
-
-    if not self.opName in self.opLocks:
-      self.opLocks[self.opName] = threading.Lock()
-    self.opLock = self.opLocks[self.opName] 
-    self.testPath = os.path.join('out_test', self.op.name)
-    self.reports = list()
-
-    self.libPath = os.path.join(self.testPath, 'libs')
-    with self.opLock:
-      if not os.path.exists(self.libPath):
-        os.makedirs(self.libPath)
-
-    self.dataPath = os.path.join(self.testPath, 'data')
-    with self.opLock:
-      if not os.path.exists(self.dataPath):
-        os.makedirs(self.dataPath)
-    hlsTCL = os.path.join('.', 'build', r'run-hls.tcl')
-
-    if args.csim:
-      self.hls = HLS(hlsTCL,True, False, False) 
-    elif args.cosim:
-      self.hls = HLS(hlsTCL, False, True, True) 
-    else:
-      self.hls = HLS(hlsTCL, self.profile['b_csim'],
-       self.profile['b_synth'], self.profile['b_cosim'])
-
-    directivePath = os.path.join(self.testPath, 
-        r'directive_par%d.tcl'%(self.parEntries))
-    self.hls.setParam(Parameters(self.op, self.logParEntries, self.parEntries))
-    with self.opLock:
-      self.hls.generateDirective(directivePath)
-
-  def build(self):
-    envD = dict()
-    parEntriesList = ['spmv', 'tpmv']
-    c_type = self.typeDict[self.op.dataType]
-    self.hls.params.setDtype(c_type)
-    envD["BLAS_dataType"] = c_type
-    self.typeStr = 'd%s'%c_type
-    if self.opClass == 'BLAS_L1':
-      r_type = self.typeDict[self.op.rtype]
-      self.typeStr = 'd%s_r%s'%(c_type, r_type)
-      envD["BLAS_resDataType"] =r_type
-      self.hls.params.setRtype(r_type)
-
-    if self.op.name in parEntriesList:
-      envD["BLAS_parEntries"] = "%d"%self.parEntries
-
-    with self.makelock:
-      make = Makefile(self.makefile, self.libPath)
-      libPath = make.make(envD)
-    self.lib = C.cdll.LoadLibrary(libPath)
-
-  def runTest(self):
-    paramTclPath =os.path.join(self.dataPath, r'parameters_%s_%s.tcl'%(self.op.sizeStr,self.typeStr))
-    logfile=os.path.join(self.dataPath, r'logfile_%s_%s.log'%(self.op.sizeStr,self.typeStr))
-    binFile =os.path.join(self.dataPath,'TestBin_%s_%s.bin'%(self.op.sizeStr,self.typeStr))
-
-    print("\n")
-    print("="*64)
-    dataList = [self.op.compute() for j in range(self.numToSim)]
-    blas_gen=BLAS_GEN(self.lib)
-    self.op.addInstr(blas_gen, dataList)
-    blas_gen.write2BinFile(binFile)
-    print("\nOP %s: Data file %s has been generated sucessfully."%(self.op.name, binFile))
-    del dataList
-    self.hls.generateParam(paramTclPath)
-    print("\nOP %s: Parameters in file %s."%(self.op.name, paramTclPath))
-    print("\nOP %s: Log file %s"%(self.op.name,  logfile))
-    self.hls.execution(binFile, logfile, self.testPath)
-    self.hls.checkLog(logfile)
-    self.numSim += self.numToSim
-    self.hls.benchmarking(logfile, self.op, self.reports)
-    print("\nOP %s: Test of size %s passed."%(self.op.name, self.op.sizeStr))
-
-  def run(self, makelock):
-    self.makelock = makelock
-    path = os.path.dirname(self.profilePath)
-    self.hls.params.setPath(path)
-    self.op.test(self)
-
-  def writeReport(self, profile, flag = 'a+'):
-    reportPath = os.path.join(self.testPath, 'report.rpt')
-    if len(self.reports) == 0:
-      raise OP_ERROR("\nOP %s: Benchmark fails for op %s."%(self.op.name, self.op.name))
-    features = self.reports[0]
-    keys = features.keys()
-    lens = [len(key)+2 for key in keys]
-
-    delimiter = '+' + '+'.join(['-' * l for l in lens]) + '+\n'
-
-    with open(reportPath, flag) as f:
-      f.write(time.ctime() + '\n')
-      f.write("Profile path: %s\n"%profile)
-      f.write(delimiter)
-      ########### START OF KEYS ################
-      strList=['|']
-      for s, l in zip(keys, lens):
-        strList.append(('{:<%d}|'%l).format(s))
-      strList.append('\n')
-      f.write(''.join(strList))
-      f.write(delimiter)
-      ########### END OF KEYS ################
-
-      while self.reports:
-        features = self.reports.pop(0)
-        strList=['|']
-        for key, l in zip(keys, lens):
-          strList.append(('{:<%d}|'%l).format(features[key]))
-        strList.append('\n')
-        f.write(''.join(strList))
-        f.write(delimiter)
-    return reportPath
 
 def makeTable(passDict, failDict, skipList, print_fn = print):
   numPasses = len(passDict)
@@ -244,23 +74,23 @@ def makeTable(passDict, failDict, skipList, print_fn = print):
   return numFails
 
 
-def process(runTest, passOps, failOps, dictLock, makeLock):
+def process(rt, passOps, failOps, dictLock, makeLock):
   passed = False
   try:
-    runTest.parseProfile()
-    with runTest.opLock:
-      print("Starting to test %s."%(runTest.op.name))
-      runTest.run(makeLock) 
-      print("All %d tests for %s are passed."%(runTest.numSim, runTest.op.name))
+    rt.parseProfile()
+    with rt.opLock:
+      print("Starting to test %s."%(rt.op.name))
+      rt.run(makeLock) 
+      print("All %d tests for %s are passed."%(rt.numSim, rt.op.name))
       passed = True
-      csim = runTest.numSim * runTest.hls.csim
-      cosim = runTest.numSim * runTest.hls.cosim
+      csim = rt.numSim * rt.hls.csim
+      cosim = rt.numSim * rt.hls.cosim
       with dictLock:
-        passOps[runTest] = (csim, cosim)
+        passOps[rt] = (csim, cosim)
 
-      if runTest.hls.cosim:
-        rpt = runTest.writeReport(profile)
-        print("Benchmark info for op %s is written in %s"%(runTest.op.name, rpt))
+      if rt.hls.cosim:
+        rpt = rt.writeReport(profile)
+        print("Benchmark info for op %s is written in %s"%(rt.op.name, rpt))
 
   except OP_ERROR as err:
     print("OPERATOR ERROR: %s"%(err.message))
@@ -273,10 +103,10 @@ def process(runTest, passOps, failOps, dictLock, makeLock):
     traceback.print_exception(type, value, tb)
   finally:
     if not passed:
-      csim = runTest.numSim * runTest.hls.csim
-      cosim = runTest.numSim * runTest.hls.cosim
+      csim = rt.numSim * rt.hls.csim
+      cosim = rt.numSim * rt.hls.cosim
       with dictLock:
-        failOps[runTest] = (csim, cosim)
+        failOps[rt] = (csim, cosim)
 
 def main(profileList, args): 
   print(r"There are in total %d testing profile[s]."%len(profileList))
@@ -287,14 +117,13 @@ def main(profileList, args):
   argList = list()
   dictLock = threading.Lock()
   makeLock = threading.Lock()
-  while profileList:
-    profile = profileList.pop()
+  for profile in profileList:
     if not os.path.exists(profile):
       print("File %s is not exists."%profile)
       skipList.append(profile)
       continue
-    runTest = RunTest(profile, args, opLocks)
-    argList.append(runTest)
+    rt = RunTest(profile, args)
+    argList.append(rt)
   try:
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.parallel) as executor:
       for arg in argList:
