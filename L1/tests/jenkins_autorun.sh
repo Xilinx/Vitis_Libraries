ef4f871a6c0d676b873da3aa5397efd4feb32394
Liang <liangm@xilinx.com>
2019-08-21 06:46:46 -0700

    submit jobs to multiple machines

--- L1/tests/jenkins_autorun.sh
+++ L1/tests/jenkins_autorun.sh
@@ -16,10 +16,11 @@ source set_env.sh
 PYTHON=python3
 PYTEST=./sw/python/run_test.py
 PAR=8
+SUBMIT=bsub -cwd `pwd` -q medium -R "select[(os== lin && type == X86_64 && (osdistro == rhel || osdistro == centos) && (osver == ws6 || osver== ws7))] rusage[mem=16000]" 
 
-$PYTHON $PYTEST --operator amax amin asum axpy copy dot nrm2 scal swap --parallel $PAR
-$PYTHON $PYTEST --operator gemv gbmv sbmvLo sbmvUp tbmvLo tbmvUp --parallel $PAR
-$PYTHON $PYTEST --operator symvLo symvUp spmvUp spmvLo tpmvLo tpmvUp trmvLo trmvUp --parallel $PAR --csim
+$SUBMIT $PYTHON $PYTEST --operator amax amin asum axpy copy dot nrm2 scal swap --parallel $PAR
+$SUBMIT $PYTHON $PYTEST --operator gemv gbmv sbmvLo sbmvUp tbmvLo tbmvUp trmvLo trmvUp --parallel $PAR
+$SUBMIT $PYTHON $PYTEST --operator symvLo symvUp spmvUp spmvLo tpmvLo tpmvUp --parallel $PAR --csim
 
 if [ -f $STAT ]; then
   cat $STAT
