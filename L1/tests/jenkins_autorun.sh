6eb130a274237ec546a594e239ebca558cd1587c
Liang <liangm@xilinx.com>
2019-08-21 09:42:13 -0700

    remove bsub from script

--- L1/tests/jenkins_autorun.sh
+++ L1/tests/jenkins_autorun.sh
@@ -17,13 +17,13 @@ PYTHON=python3
 PYTEST=./sw/python/run_test.py
 PYCHECK=./sw/python/check_process.py
 PAR=8
-set SUBMIT 'bsub -cwd `pwd` -q medium -R "select[(os== lin && type == X86_64 && (osdistro == rhel ||osdistro == centos) && (osver == ws6 || osver== ws7))] rusage[mem=16000]"'
+#SUBMIT="bsub -cwd `pwd` -q medium -R \"select[(os== lin && type == X86_64 && (osdistro == rhel||osdistro == centos) && (osver == ws6 || osver== ws7))] rusage[mem=16000]\" "
 
-$SUBMIT $PYTHON $PYTEST --operator amax amin asum axpy --parallel $PAR --id 0  --csim &
-$SUBMIT $PYTHON $PYTEST --operator copy dot nrm2 scal swap --parallel $PAR  --id 1 --csim &
-$SUBMIT $PYTHON $PYTEST --operator gemv trmvLo trmvUp --parallel $PAR  --id 2 --csim &
-$SUBMIT $PYTHON $PYTEST --operator gbmv sbmvLo sbmvUp tbmvLo tbmvUp --parallel $PAR  --id 3 --csim &
-$SUBMIT $PYTHON $PYTEST --operator symvLo symvUp spmvUp spmvLo tpmvLo tpmvUp --parallel $PAR --csim  --id 4&
+$PYTHON $PYTEST --operator amax amin asum axpy --parallel $PAR --id 0  --csim &
+$PYTHON $PYTEST --operator copy dot nrm2 scal swap --parallel $PAR  --id 1 --csim &
+$PYTHON $PYTEST --operator gemv trmvLo trmvUp --parallel $PAR  --id 2 --csim &
+$PYTHON $PYTEST --operator gbmv sbmvLo sbmvUp tbmvLo tbmvUp --parallel $PAR  --id 3 --csim &
+$PYTHON $PYTEST --operator symvLo symvUp spmvUp spmvLo tpmvLo tpmvUp --parallel $PAR --csim  --id 4&
 $PYTHON $PYCHECK --basename "statistics" --ext "rpt" --number 5
 
 if [ -f $STAT ]; then
