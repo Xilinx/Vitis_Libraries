22eaf187466bd2a54548a4938ea7aac6e3adc790
Liang <liangm@xilinx.com>
2019-08-22 07:05:22 -0700

    add check_process from master branch

---
+++ L1/tests/sw/python/check_process.py
@@ -0,0 +1,37 @@
+import time
+import functools
+import argparse
+import os
+
+def poll(fileList, t):
+  while True:
+    fileFound = [os.path.exists(f) for f in fileList]
+    func = lambda x, y : x and y
+    allFound = functools.reduce(func, fileFound)
+    if allFound:
+      print("Poll finished.")
+      break
+    print("Poll sleep for %ds."%t)
+    time.sleep(t)
+
+def merge(fileList, filename):
+  with open(filename, 'w+') as f:
+    for file in fileList:
+      with open(file, 'r+') as fr:
+        f.write(fr.read())
+
+def main(args): 
+  fileList = ['%s_%d.%s'%(args.basename, i, args.ext) for i in range(args.number) ]
+  poll(fileList, args.time)
+  merge(fileList, '%s.%s'%(args.basename,args.ext))
+
+if __name__== "__main__":
+  parser = argparse.ArgumentParser(description='Generate random vectors and run test.')
+  parser.add_argument('--basename', type=str, default='statistics', help='filename to check')
+  parser.add_argument('--number', type=int, required=True, help='number of files')
+  parser.add_argument('--ext', type=str, default='rpt', help='file extension')
+  parser.add_argument('--time', type=int, default=600, help='number of seconds to poll')
+  args = parser.parse_args()
+  
+  main(args)
+
