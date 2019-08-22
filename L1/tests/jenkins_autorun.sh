8525f44ea80609792e6507eef9f24f7782f6b589
Liang <liangm@xilinx.com>
2019-08-22 11:27:51 -0700

    correct the env settings

--- L1/tests/jenkins_autorun.sh
+++ L1/tests/jenkins_autorun.sh
@@ -19,10 +19,10 @@ PYTHON=python3
 PYTEST=./sw/python/run_test.py
 PYCHECK=./sw/python/check_process.py
 PAR=4
-SUBMIT="bsub -cwd `pwd` -q medium -R \"select[(os== lin && type == X86_64 && (osdistro == rhel || osdistro == centos) && (osver == ws6 || osver== ws7))] rusage[mem=16000]\""
 
+SUBMIT="bsub -cwd `pwd` -q medium -R \"select[(os== lin && type == X86_64 && (osdistro == rhel || osdistro == centos) && (osver== ws7))] rusage[mem=16000]\""
 eval "$SUBMIT $PYTHON $PYTEST --operator amax amin asum axpy copy dot nrm2 scal swap --parallel $PAR --id 0 &"
-eval "$SUBMIT $PYTHON $PYTEST --operator gemv gbmv sbmvLo sbmvUp tbmvLo tbmvUp trmvLo trmvUp --parallel $PAR --id 1&"
+eval "$SUBMIT $PYTHON $PYTEST --operator gemv gbmv sbmvLo sbmvUp tbmvLo tbmvUp trmvLo trmvUp --parallel $PAR --id 1 &"
 eval "$SUBMIT $PYTHON $PYTEST --operator symvLo symvUp spmvUp spmvLo tpmvLo tpmvUp --parallel $PAR --csim --id 2 &"
 $PYTHON $PYCHECK --number 3
 
