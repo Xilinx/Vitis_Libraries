25cb2a710751241941fdcff020724d9b8bf73898
Liang <liangm@xilinx.com>
2019-08-23 03:16:33 -0700

    partition L2 functions into two tests

--- L1/tests/jenkins_autorun.sh
+++ L1/tests/jenkins_autorun.sh
@@ -19,13 +19,14 @@ PYTHON=python3
 PYTEST=./sw/python/run_test.py
 PYCHECK=./sw/python/check_process.py
 
-PAR=8
+PAR=4
 id=0
 
 SUBMIT="bsub -cwd `pwd` -q medium -R \"select[(os== lin && type == X86_64 && (osdistro == rhel || osdistro == centos) && (osver== ws7))] rusage[mem=16000]\""
 
 OPLIST=("amax amin asum axpy copy dot nrm2 scal swap" \
-    "gemv gbmv sbmvLo sbmvUp tbmvLo tbmvUp trmvLo trmvUp" \
+    "gemv gbmv" \
+    "sbmvLo sbmvUp tbmvLo tbmvUp trmvLo trmvUp" \
     "symvLo symvUp spmvUp spmvLo tpmvLo tpmvUp --csim")
 
 for val in "${OPLIST[@]}"; do
