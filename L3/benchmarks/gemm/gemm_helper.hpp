ef3c93cb2c07aa41c928e8018dffa5fca06eaa89
Hsin-Yu Ting <hsinyut@xilinx.com>
2019-09-06 14:22:03 -0700

    clean redudant codes in L3 bench

--- L3/benchmarks/gemm/gemm_helper.hpp
+++ L3/benchmarks/gemm/gemm_helper.hpp
@@ -65,31 +65,4 @@ bool compareGemm(
     return l_check;
 }
 
-bool readConfigDict(string p_configFile, unordered_map<string, string>* p_configDict) {
-    unordered_map<string, string> l_configDict;
-    ifstream l_configInfo(p_configFile);
-    bool l_good = l_configInfo.good();
-    if (!l_good) {
-        return false;
-    }
-    if (l_configInfo.is_open()) {
-        string line;
-        string key;
-        string value;
-        string equalSign = "=";
-        while (getline(l_configInfo, line)) {
-            int index = line.find(equalSign);
-            if (index == 0) continue;
-            key = line.substr(0, index);
-            value = line.substr(index + 1);
-            l_configDict[key] = value;
-        }
-    }
-
-    l_configInfo.close();
-
-    *p_configDict = l_configDict;
-    return true;
-}
-
 #endif
