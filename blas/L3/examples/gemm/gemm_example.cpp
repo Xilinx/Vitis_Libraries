cfbbc39dafcf2801c4aec49f8acf740af5da23c9
Stant <laurence.stant@diamond.ac.uk>
2019-10-02 10:35:52 +0100

    fixed typos in blas l3 examples

--- blas/L3/examples/gemm/gemm_example.cpp
+++ blas/L3/examples/gemm/gemm_example.cpp
@@ -165,7 +165,7 @@ int main(int argc, char** argv) {
     status = xfblasGetMatrixRestricted(c, l_numKernel - 1);
 
     if (status != XFBLAS_STATUS_SUCCESS) {
-        cout << "Get Matirx failed with error code: " << status << "\n";
+        cout << "Get Matrix failed with error code: " << status << "\n";
         xfblasDestroy();
         return EXIT_FAILURE;
     }
