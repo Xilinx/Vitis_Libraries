d90b4aba7e36447a06148f5fac4adefc100c2048
Lisa Liu <lingl@xilinx.com>
2019-10-30 04:44:00 -0700

    merge gemm compute implementation and L1 internal tests from dev branch

---
+++ L1/tests/internal/transp/parameters.tcl
@@ -0,0 +1,24 @@
+array set opt {
+    dataType  uint32_t 
+    resDataType int32_t
+    logParEntries 2
+    parEntries 4
+    parBlocks 1
+    L2 true
+    opName "sbmvUp"
+    matrixSize 256
+    vectorSize 16
+    memorySize 64
+    runCsim    1 
+    runRTLsynth   0
+    runRTLsim     0
+    part    vu9p
+    pageSizeBytes 4096
+    memWidthBytes 64
+    instrSizeBytes 8
+    maxNumInstrs 16
+    instrPageIdx 0
+    paramPageIdx 1
+    statsPageIdx 2
+  }
+ 
---
+++ L1/tests/internal/transp/parametersMem.tcl
@@ -0,0 +1,11 @@
+array set opt {
+    dataType  uint32_t 
+    memWidth 4 
+    cols 12
+    rows 8
+    runCsim    1 
+    runRTLsynth   0
+    runRTLsim     0
+    part    vu9p
+  }
+ 
