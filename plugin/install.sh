37b5e4a9750d27acad4fed148cbf26b197bbe2a4
fengx <fengx@xilinx.com>
2020-12-03 03:02:57 -0700

    Squashed 'graph/' changes from 369080d..b97e697
    b97e697 Merge pull request #342 from ywu/master
    b3a7f38 Removed lsf reference from README.md
    2ffbf93 Merge branch 'master' of https://gitenterprise.xilinx.com/ywu/xf_graph
    b814d21 Removed xclbins from the git repo; Added checking for xclbin loading failures
    7603d2f Update README.md
    badfccf Update README.md
    66ab9ff Added copying bash_tigergraph to the home directory
    15eb447 TigerGraph plugin with EoU updates
    
    git-subtree-dir: graph
    git-subtree-split: b97e69793ae1c8381e06d131746c4d0f70502c1a

--- plugin/install.sh
+++ plugin/install.sh
@@ -1,29 +1,31 @@
-TigerGraphPath=/home/tigergraph/tigergraph
+#!/bin/bash
+
+echo "Checking TigerGraph installation directory"
+if [ ! -f "$HOME/.gsql/gsql.cfg" ]; then
+    echo "Please run this install script as the user for the TigerGraph installation."
+fi
+tg_root_dir_kv=$(grep tigergraph.root.dir $HOME/.gsql/gsql.cfg)
+tg_root_dir=$(cut -d' ' -f2 <<< $tg_root_dir_kv)
+echo "Found TigerGraph installation in $tg_root_dir"
 
 rm -rf tigergraph/QueryUdf/tgFunctions.hpp
-
 rm -rf tigergraph/QueryUdf/ExprFunctions.hpp
-
 rm -rf tigergraph/QueryUdf/ExprUtil.hpp
-
 rm -rf tigergraph/QueryUdf/graph.hpp
 
-sudo cp -rf $TigerGraphPath/dev/gdk/gsql/src/QueryUdf $TigerGraphPath/dev/gdk/gsql/src/bck_QueryUdf
+# save a copy of the original UDF Files
+if [ ! -d "$tg_root_dir/dev/gdk/gsql/src/QueryUdf.orig" ]; then
+    cp -r $tg_root_dir/dev/gdk/gsql/src/QueryUdf $tg_root_dir/dev/gdk/gsql/src/QueryUdf.orig
+    echo "Saved a copy of the original QueryUdf files in $tg_root_dir/gdk/gsql/src/QueryUdf.orig"
+fi
 
-sudo cp -rf $TigerGraphPath/dev/gdk/MakeUdf $TigerGraphPath/dev/gdk/bck_MakeUdf
-
-sudo cp -rf $TigerGraphPath/../.bash_tigergraph $TigerGraphPath/../.bck_bash_tigergraph
-
-sudo cp $TigerGraphPath/dev/gdk/gsql/src/QueryUdf/ExprFunctions.hpp tigergraph/QueryUdf/tgFunctions.hpp
-
-sudo cp $TigerGraphPath/dev/gdk/gsql/src/QueryUdf/ExprUtil.hpp tigergraph/QueryUdf
-
+cp $tg_root_dir/dev/gdk/gsql/src/QueryUdf.orig/ExprFunctions.hpp tigergraph/QueryUdf/tgFunctions.hpp
+cp $tg_root_dir/dev/gdk/gsql/src/QueryUdf.orig/ExprUtil.hpp tigergraph/QueryUdf
 cp tigergraph/QueryUdf/xilinxUdf.hpp tigergraph/QueryUdf/ExprFunctions.hpp
 
 cp -rf ../L3/include/graph.hpp tigergraph/QueryUdf
 
 xrtPath=/opt/xilinx/xrt
-
 xrmPath=/opt/xilinx/xrm
 
 while getopts ":r:m:" opt
@@ -44,21 +46,35 @@ case $opt in
 done
 
 source $xrtPath/setup.sh
-
 source $xrmPath/setup.sh
 
-make clean
+#make clean
+make TigerGraphPath=$tg_root_dir libgraphL3wrapper
 
-make libgraphL3wrapper
+#rm -rf $tg_install_dir/tigergraph/dev/gdk/gsql/src/QueryUdf
+cp -rf tigergraph/QueryUdf/* $tg_root_dir/dev/gdk/gsql/src/QueryUdf
+cp -rf tigergraph/MakeUdf $tg_root_dir/dev/gdk/
 
-sudo chmod 777 -R tigergraph
+# update files with tg_root_dir
+for f in $tg_root_dir/dev/gdk/gsql/src/QueryUdf/*.json; do
+    # use | as the demiliter since tg_root_dir has / in it
+    sed -i "s|TG_ROOT_DIR|$tg_root_dir|" $f 
+done
+sed -i "s|TG_ROOT_DIR|$tg_root_dir|" $tg_root_dir/dev/gdk/MakeUdf 
 
-sudo rm -rf $TigerGraphPath/dev/gdk/gsql/src/QueryUdf
+# Back up .bash_tigergraph
+if [ -f "$HOME/.bash_tigergraph" ]; then
+    backup_file=".bash_tigergraph-$(date "+%Y%m%d-%H%M%S")"
+    cp $HOME/.bash_tigergraph $HOME/$backup_file
+    #cp -r $tg_root_dir/dev/gdk/gsql/src/QueryUdf $tg_root_dir/dev/gdk/gsql/src/QueryUdf.orig
+    echo "Saved the original $HOME/.bash_tigergraph as $HOME/$backup_file"
+fi
+cp tigergraph/bash_tigergraph $HOME/.bash_tigergraph
 
-sudo cp -rf tigergraph/QueryUdf $TigerGraphPath/dev/gdk/gsql/src/
+echo "Xilinx FPGA acceleration plugin for Tigergraph has been installed."
 
-sudo cp -rf tigergraph/MakeUdf $TigerGraphPath/dev/gdk/
-
-sudo cp -rf tigergraph/bash_tigergraph $TigerGraphPath/../.bash_tigergraph
-
-sudo chmod 777 -R $TigerGraphPath/dev/gdk/gsql/src/QueryUdf
+# Copy xclbins to TG root directory 
+echo "Xilinx FPGA binary files for accelerated graph functions need to be dowloaded"
+echo "from Xilinx DBA lounge and then installed by following instructions in the package."
+#mkdir -p $tg_root_dir/dev/gdk/gsql/src/QueryUdf/xclbin
+#cp xclbin/xilinx_u50_gen3x16_xdma_201920_3/*.xclbin $tg_root_dir/dev/gdk/gsql/src/QueryUdf/xclbin 
