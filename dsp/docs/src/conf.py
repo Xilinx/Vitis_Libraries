4f49c2e8b562cc373d1fa0f5d0352f2ee4e1198b
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2021-07-19 09:09:44 +0800

    Squashed 'dsp/' changes from 39ad7b8..0096966 (#363)

--- dsp/docs/src/conf.py
+++ dsp/docs/src/conf.py
@@ -26,13 +26,13 @@ sys.path.insert(1, os.path.join(tools_dir, 'share/doxyrest_b/sphinx'))
 # -- Project information -----------------------------------------------------
 
 project = 'Vitis DSP Library'
-copyright = '2019-2020, Xilinx Inc.'
+copyright = '2019-2021, Xilinx Inc.'
 author = 'Xilinx Inc.'
 
 # The short X.Y version
-version = '2020.2'
+version = '2021.1'
 # The full version, including alpha/beta/rc tags
-release = 'v2020.2'
+release = 'v2021.1'
 
 
 # -- General configuration ---------------------------------------------------
