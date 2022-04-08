61a64e9cf27142964a2b09a8751341757126389c
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2022-04-08 09:50:08 +0800

    Squashed 'sparse' changes from a0f15de..b8aa0ac (#545)
    b8aa0ac Merge pull request #98 from changg/round2-mk
    0dafa9e fix
    69ce1de Merge pull request #97 from changg/round2-mk
    33635fb fix utils.mk
    bb09f85 Merge pull request #96 from tuol/fix_cr_1125397
    26bfd12 upate description.json for GUI
    6142bf4 Merge pull request #95 from tuol/fix_cr1110852
    18b6b69 add description for cscmvSingleHBM
    489d6fe Merge pull request #94 from changg/wa_u280
    c83ed11 WA for u280
    70d666c Revert "wa for xilinx_u280_xdma_201920_3"
    27306c5 Merge pull request #93 from changg/wa_u280_2019
    2b4ec88 wa for xilinx_u280_xdma_201920_3
    af1d094 Merge pull request #92 from liyuanz/replace_cflags
    a9ff957 replace cflags with clflags
    eb58a83 Merge pull request #91 from tuol/fix_cr_1110852
    c9ca9ab add description to L2/tests/cscmvSingleHbm
    b8e0210 Merge pull request #90 from tuol/fix_cr_1083211
    75204c8 remove 'exclude' from description.json
    965b2e0 Merge pull request #88 from liyuanz/replace_blacklist
    0e87eee replace whiltelist/blacklist to allowlist/blocklist
    ad26de7 Merge pull request #87 from liyuanz/next
    3db258c increase time
    def14fe Merge pull request #85 from changg/add_extraflags
    f8a6122 fix makefile
    031de2c Merge pull request #84 from liyuanz/next
    f10953c increase time
    4745f24 Merge pull request #83 from changg/fix_utils
    6fb2652 fix utisl
    7d9278f Merge pull request #82 from liyuanz/replace_targets
    c1c2de5 update targes
    68ae52e Merge pull request #80 from changg/metadata
    6225d51 draft metadata files
    e74cf2b change 2021.2_stable_latest to 2022.1_stable_latest
    
    Co-authored-by: sdausr <sdausr@xilinx.com>

--- sparse/L2/tests/cscmvSingleHbm/utils.mk
+++ sparse/L2/tests/cscmvSingleHbm/utils.mk
@@ -73,18 +73,25 @@ endif
 
 #Checks for g++
 CXX := g++
+VITIS_VER = $(shell v++ --version | grep 'v++' | sed 's/^[[:space:]]*//' | sed -e 's/^[*]* v++ v//g' | cut -d " " -f1)
 ifeq ($(HOST_ARCH), x86)
-ifneq ($(shell expr $(shell g++ -dumpversion) \>= 5), 1)
-ifndef XILINX_VIVADO
-$(error [ERROR]: g++ version older. Please use 5.0 or above)
+ifeq ($(shell expr $(VITIS_VER) \>= 2022.1), 1)
+CXX_VER := 8.3.0
 else
-CXX := $(XILINX_VIVADO)/tps/lnx64/gcc-6.2.0/bin/g++
-ifeq ($(LD_LIBRARY_PATH),)
-export LD_LIBRARY_PATH := $(XILINX_VIVADO)/tps/lnx64/gcc-6.2.0/lib64
-else
-export LD_LIBRARY_PATH := $(XILINX_VIVADO)/tps/lnx64/gcc-6.2.0/lib64:$(LD_LIBRARY_PATH)
+CXX_VER := 6.2.0
 endif
-$(warning [WARNING]: g++ version older. Using g++ provided by the tool : $(CXX))
+CXX_V := $(shell echo $(CXX_VER) | awk -F. '{print tolower($$1)}')
+ifneq ($(shell expr $(shell echo "__GNUG__" | g++ -E -x c++ - | tail -1) \>= $(CXX_V)), 1)
+ifndef XILINX_VIVADO
+$(error [ERROR]: g++ version too old. Please use $(CXX_VER) or above)
+else
+CXX := $(XILINX_VIVADO)/tps/lnx64/gcc-$(CXX_VER)/bin/g++
+ifeq ($(LD_LIBRARY_PATH),)
+export LD_LIBRARY_PATH := $(XILINX_VIVADO)/tps/lnx64/gcc-$(CXX_VER)/lib64
+else
+export LD_LIBRARY_PATH := $(XILINX_VIVADO)/tps/lnx64/gcc-$(CXX_VER)/lib64:$(LD_LIBRARY_PATH)
+endif
+$(warning [WARNING]: g++ version too old. Using g++ provided by the tool: $(CXX))
 endif
 endif
 else ifeq ($(HOST_ARCH), aarch64)
@@ -199,3 +206,8 @@ RMDIR = rm -rf
 MV = mv -f
 CP = cp -rf
 ECHO:= @echo
+ifneq (,$(shell echo $(XPLATFORM) | awk '/xilinx_u280_xdma_201920_3/'))
+ifeq ($(TARGET), hw)
+VPP_LDFLAGS += --advanced.param compiler.userPreSysLinkOverlayTcl=preSysLink.tcl
+endif
+endif
