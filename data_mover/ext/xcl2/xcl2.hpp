75225b0aaff76348ea3ba0e2b121c9d2f34ea15a
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2023-05-10 09:34:27 +0800

    Squashed 'data_mover' changes from 8464630..0400d57 (#849)
    0400d57 Merge pull request #13 from tuol/next
    6f6c9a2 add copyright/license for several files
    46eb082 Merge pull request #9 from changg/fix_json
    3bd965c Merge pull request #11 from changg/fix_conflict
    c8411d9 update copyright, fix conflict
    a68e7b4 fix json file, otherwise makefile will use a wrong default platform
    
    Co-authored-by: sdausr <sdausr@xilinx.com>

--- data_mover/ext/xcl2/xcl2.hpp
+++ data_mover/ext/xcl2/xcl2.hpp
@@ -1,5 +1,6 @@
 /**********
-Copyright (c) 2018, Xilinx, Inc.
+Copyright (C) 2019-2022, Xilinx, Inc.
+Copyright (C) 2022-2023, Advanced Micro Devices, Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
