93476a902b19f110b75040e4524cff62d06de446
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2023-08-15 16:52:46 +0800

    Squashed 'security' changes from 12c3a5b..358c7e9 (#896)
    358c7e9 Merge pull request #228 from RepoOps/domain_visible_a
    a429af5 update
    624357c update api meta json
    f401a45 Merge pull request #227 from RepoOps/fix_u2_
    6bad0a7 update
    8142dcd update
    dd03077 fix u2
    17bf766 Merge pull request #226 from liyuanz/next
    b10d9f9 update
    59745d5 update
    55f7283 Merge pull request #225 from liyuanz/next
    178c30f update makefile and add config file
    36965d9 change 2023.1_stable_latest to 2023.2_stable_latest
    
    Co-authored-by: sdausr <sdausr@xilinx.com>

--- security/L3/tests/crc32c_sc/utils.mk
+++ security/L3/tests/crc32c_sc/utils.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2019-2022, Xilinx, Inc.
-# Copyright (C) 2022-2023, Advanced Micro Devices, Inc.
+# Copyright 2019-2021 Xilinx, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
