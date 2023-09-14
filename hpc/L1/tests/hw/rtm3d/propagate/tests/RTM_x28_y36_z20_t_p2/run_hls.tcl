9b39a65bf096bf812e1c854205340bf068ce04fb
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2023-09-14 17:28:38 +0800

    Squashed 'hpc' changes from 72169a5..340c103 (#930)
    340c103 Merge pull request #182 from liyuanz/next
    aadce96 update
    0cd19e3 Merge pull request #181 from liyuanz/next
    1ebc5e2 update
    
    Co-authored-by: sdausr <sdausr@xilinx.com>

--- hpc/L1/tests/hw/rtm3d/propagate/tests/RTM_x28_y36_z20_t_p2/run_hls.tcl
+++ hpc/L1/tests/hw/rtm3d/propagate/tests/RTM_x28_y36_z20_t_p2/run_hls.tcl
@@ -14,7 +14,14 @@
 # limitations under the License.
 # vitis hls makefile-generator v2.0.0
 
-source settings.tcl
+set CSIM 1
+set CSYNTH 1
+set COSIM 1
+set VIVADO_SYN 1
+set VIVADO_IMPL 1
+set CUR_DIR [pwd]
+set XF_PROJ_ROOT $CUR_DIR/../../../../../../..
+set XPART xcu280-fsvh2892-2L-e
 
 set PROJ "propagate_RTM_x28_y36_z20_t_p2_test.prj"
 set SOLN "sol"
