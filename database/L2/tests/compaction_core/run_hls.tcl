61bc7d3752d580c886944e39f243465569fc81f3
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2023-09-15 11:03:11 +0800

    Squashed 'database' changes from d02f845..9a0d474 (#929)
    9a0d474 update (#814)
    a41b3f6 Fix route failure of L1/benchmark/compound_sort (#813)
    cc17d81 update tcl (#812)
    f92f27b Add strategy, Performance_SpreadSLLs (#810)
    
    Co-authored-by: sdausr <sdausr@xilinx.com>

--- database/L2/tests/compaction_core/run_hls.tcl
+++ database/L2/tests/compaction_core/run_hls.tcl
@@ -14,7 +14,14 @@
 # limitations under the License.
 #
 
-source settings.tcl
+set CSIM 1
+set CSYNTH 1
+set COSIM 1
+set VIVADO_SYN 1
+set VIVADO_IMPL 1
+set CUR_DIR [pwd]
+set XF_PROJ_ROOT $CUR_DIR/../../..
+set XPART 
 
 set PROJ "compaction_test.prj"
 set SOLN "sol1"
