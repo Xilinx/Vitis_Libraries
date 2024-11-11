bb1bd3b6be7d96e0bef49ef735bb409e732871bc
SDA USR <1391+sdausr@users.noreply.gitenterprise.xilinx.com>
2024-11-11 14:55:08 +0800

    Squashed 'blas' changes from d3199e03..5231ca8a (#1122)
    5231ca8a Merge pull request #238 from RepoOps/update_20241029-202327
    0762e838 clang format
    
    Co-authored-by: sdausr <sdausr@xilinx.com>

--- blas/L1/include/hw/xf_blas/gemm.hpp
+++ blas/L1/include/hw/xf_blas/gemm.hpp
@@ -33,138 +33,127 @@ namespace xf {
 
 namespace blas {
 
-template <typename t_DataType, unsigned int t_KBufferDim,
+template <typename t_DataType,
+          unsigned int t_KBufferDim,
           unsigned int t_ParEntriesM,
           unsigned int t_ParEntriesN = t_ParEntriesM,
           typename t_MacDataType = t_DataType>
 class Gemm {
-  typedef WideType<t_DataType, t_ParEntriesM> WideTypeM;
-  typedef WideType<t_DataType, t_ParEntriesN> WideTypeN;
-  typedef WideType<t_MacDataType, t_ParEntriesN> WideMacTypeN;
+    typedef WideType<t_DataType, t_ParEntriesM> WideTypeM;
+    typedef WideType<t_DataType, t_ParEntriesN> WideTypeN;
+    typedef WideType<t_MacDataType, t_ParEntriesN> WideMacTypeN;
 
-  typedef hls::stream<typename WideTypeM::t_TypeInt> EdgeStreamM;
-  typedef hls::stream<typename WideTypeN::t_TypeInt> EdgeStreamN;
-  typedef hls::stream<typename WideMacTypeN::t_TypeInt> EdgeMacStreamN;
+    typedef hls::stream<typename WideTypeM::t_TypeInt> EdgeStreamM;
+    typedef hls::stream<typename WideTypeN::t_TypeInt> EdgeStreamN;
+    typedef hls::stream<typename WideMacTypeN::t_TypeInt> EdgeMacStreamN;
 
-  typedef TaggedFloat<t_DataType> TaggedDataType;
+    typedef TaggedFloat<t_DataType> TaggedDataType;
 
-  typedef TaggedFloat<t_MacDataType> TaggedMacType;
-  typedef WideType<typename TaggedMacType::t_TypeInt, t_ParEntriesN>
-      WideTaggedMacType;
-  typedef hls::stream<typename WideTaggedMacType::t_TypeInt>
-      TaggedMacTypeStream;
+    typedef TaggedFloat<t_MacDataType> TaggedMacType;
+    typedef WideType<typename TaggedMacType::t_TypeInt, t_ParEntriesN> WideTaggedMacType;
+    typedef hls::stream<typename WideTaggedMacType::t_TypeInt> TaggedMacTypeStream;
 
-public:
-  static void gemm(EdgeStreamM &p_As, EdgeStreamN &p_Bs, EdgeMacStreamN &p_sum,
-                   unsigned int p_blocks) {
+   public:
+    static void gemm(EdgeStreamM& p_As, EdgeStreamN& p_Bs, EdgeMacStreamN& p_sum, unsigned int p_blocks) {
 #ifndef __SYNTHESIS__
-    assert(t_KBufferDim >= t_ParEntriesM + t_ParEntriesN);
+        assert(t_KBufferDim >= t_ParEntriesM + t_ParEntriesN);
 #endif
 
-    WindowRm<TaggedDataType, t_ParEntriesM, t_ParEntriesN> l_awin;
-    WindowRm<TaggedDataType, t_ParEntriesN, t_ParEntriesM> l_bwin;
-    TriangSrl<TaggedDataType, t_ParEntriesM> l_Ta;
-    TriangSrl<TaggedDataType, t_ParEntriesN> l_Tb;
-    l_awin.clear();
-    l_bwin.clear();
-    l_Ta.clear();
-    l_Tb.clear();
+        WindowRm<TaggedDataType, t_ParEntriesM, t_ParEntriesN> l_awin;
+        WindowRm<TaggedDataType, t_ParEntriesN, t_ParEntriesM> l_bwin;
+        TriangSrl<TaggedDataType, t_ParEntriesM> l_Ta;
+        TriangSrl<TaggedDataType, t_ParEntriesN> l_Tb;
+        l_awin.clear();
+        l_bwin.clear();
+        l_Ta.clear();
+        l_Tb.clear();
 
-    typedef WideType<TaggedDataType, t_ParEntriesM> TaggedArrayM;
-    typedef WideType<TaggedDataType, t_ParEntriesN> TaggedArrayN;
+        typedef WideType<TaggedDataType, t_ParEntriesM> TaggedArrayM;
+        typedef WideType<TaggedDataType, t_ParEntriesN> TaggedArrayN;
 
-    WideType<t_MacDataType, t_ParEntriesN> l_C[t_ParEntriesM];
+        WideType<t_MacDataType, t_ParEntriesN> l_C[t_ParEntriesM];
 #pragma HLS ARRAY_PARTITION variable = l_C dim = 1 complete
-    WideType<t_MacDataType, t_ParEntriesN> l_Co[t_ParEntriesM];
+        WideType<t_MacDataType, t_ParEntriesN> l_Co[t_ParEntriesM];
 #pragma HLS ARRAY_PARTITION variable = l_Co dim = 1 complete
 
-    for (uint32_t l = 0; l <= p_blocks; l++)
-      for (int k = 0; k < t_KBufferDim; k++) {
+        for (uint32_t l = 0; l <= p_blocks; l++)
+            for (int k = 0; k < t_KBufferDim; k++) {
 #pragma HLS PIPELINE
 
-        WideType<t_DataType, t_ParEntriesM> l_A = 0;
-        WideType<t_DataType, t_ParEntriesN> l_B = 0;
+                WideType<t_DataType, t_ParEntriesM> l_A = 0;
+                WideType<t_DataType, t_ParEntriesN> l_B = 0;
 
-        if (l < p_blocks) {
-          l_A = p_As.read();
-          l_B = p_Bs.read();
-        }
+                if (l < p_blocks) {
+                    l_A = p_As.read();
+                    l_B = p_Bs.read();
+                }
 
-        TaggedArrayM l_avec;
-        for (int i = 0; i < t_ParEntriesM; i++)
-          l_avec[i] = TaggedDataType(l_A[i], k == 0);
-        TaggedArrayN l_bvec;
-        for (int i = 0; i < t_ParEntriesN; i++)
-          l_bvec[i] = TaggedDataType(l_B[i], k == 0);
+                TaggedArrayM l_avec;
+                for (int i = 0; i < t_ParEntriesM; i++) l_avec[i] = TaggedDataType(l_A[i], k == 0);
+                TaggedArrayN l_bvec;
+                for (int i = 0; i < t_ParEntriesN; i++) l_bvec[i] = TaggedDataType(l_B[i], k == 0);
 
-        TaggedArrayM l_avec1 = l_Ta.shift(l_avec);
-        TaggedArrayN l_bvec1 = l_Tb.shift(l_bvec);
+                TaggedArrayM l_avec1 = l_Ta.shift(l_avec);
+                TaggedArrayN l_bvec1 = l_Tb.shift(l_bvec);
 
-        (void)l_awin.shift_right(l_avec1);
-        (void)l_bwin.shift(l_bvec1);
+                (void)l_awin.shift_right(l_avec1);
+                (void)l_bwin.shift(l_bvec1);
 
-        if (l > 0 && k >= t_ParEntriesN + 1 &&
-            k <= t_ParEntriesM + t_ParEntriesN) {
-          p_sum.write(l_Co[k - t_ParEntriesN - 1]);
-        }
+                if (l > 0 && k >= t_ParEntriesN + 1 && k <= t_ParEntriesM + t_ParEntriesN) {
+                    p_sum.write(l_Co[k - t_ParEntriesN - 1]);
+                }
 
-        for (unsigned int row = 0; row < t_ParEntriesM; ++row) {
+                for (unsigned int row = 0; row < t_ParEntriesM; ++row) {
 #pragma HLS UNROLL
-          TaggedArrayM l_arow = l_awin[row];
-          TaggedArrayN l_brow = l_bwin[row];
-          for (unsigned int col = 0; col < t_ParEntriesN; ++col) {
+                    TaggedArrayM l_arow = l_awin[row];
+                    TaggedArrayN l_brow = l_bwin[row];
+                    for (unsigned int col = 0; col < t_ParEntriesN; ++col) {
 #pragma HLS UNROLL
-            t_DataType aval = l_arow[col]();
-            t_DataType bval = l_brow[col]();
-            bool aflush = l_arow[col].getFlush();
+                        t_DataType aval = l_arow[col]();
+                        t_DataType bval = l_brow[col]();
+                        bool aflush = l_arow[col].getFlush();
 #ifndef __SYNTEHSIS__
-            bool bflush = l_brow[col].getFlush();
-            assert(aflush == bflush);
+                        bool bflush = l_brow[col].getFlush();
+                        assert(aflush == bflush);
 #endif
-            if (aflush) {
-              l_Co[row][col] = l_C[row][col];
-              l_C[row][col] = 0;
+                        if (aflush) {
+                            l_Co[row][col] = l_C[row][col];
+                            l_C[row][col] = 0;
+                        }
+                        l_C[row][col] += aval * bval;
+                    }
+                }
             }
-            l_C[row][col] += aval * bval;
-          }
-        }
-      }
-  }
+    }
 };
 
-template <unsigned int t_KBufferDim, unsigned int t_ParEntriesM,
-          unsigned int t_ParEntriesN>
+template <unsigned int t_KBufferDim, unsigned int t_ParEntriesM, unsigned int t_ParEntriesN>
 class Gemm<float, t_KBufferDim, t_ParEntriesM, t_ParEntriesN, float> {
-  typedef float t_DataType;
-  typedef float t_MacDataType;
-  typedef WideType<t_DataType, t_ParEntriesM> WideTypeM;
-  typedef WideType<t_DataType, t_ParEntriesN> WideTypeN;
-  typedef WideType<t_MacDataType, t_ParEntriesN> WideMacTypeN;
+    typedef float t_DataType;
+    typedef float t_MacDataType;
+    typedef WideType<t_DataType, t_ParEntriesM> WideTypeM;
+    typedef WideType<t_DataType, t_ParEntriesN> WideTypeN;
+    typedef WideType<t_MacDataType, t_ParEntriesN> WideMacTypeN;
 
-  typedef hls::stream<typename WideTypeM::t_TypeInt> EdgeStreamM;
-  typedef hls::stream<typename WideTypeN::t_TypeInt> EdgeStreamN;
-  typedef hls::stream<typename WideMacTypeN::t_TypeInt> EdgeMacStreamN;
+    typedef hls::stream<typename WideTypeM::t_TypeInt> EdgeStreamM;
+    typedef hls::stream<typename WideTypeN::t_TypeInt> EdgeStreamN;
+    typedef hls::stream<typename WideMacTypeN::t_TypeInt> EdgeMacStreamN;
 
-  typedef TaggedFloat<t_DataType> TaggedDataType;
+    typedef TaggedFloat<t_DataType> TaggedDataType;
 
-  typedef TaggedFloat<t_MacDataType> TaggedMacType;
-  typedef WideType<typename TaggedMacType::t_TypeInt, t_ParEntriesN>
-      WideTaggedMacType;
-  typedef hls::stream<typename WideTaggedMacType::t_TypeInt>
-      TaggedMacTypeStream;
+    typedef TaggedFloat<t_MacDataType> TaggedMacType;
+    typedef WideType<typename TaggedMacType::t_TypeInt, t_ParEntriesN> WideTaggedMacType;
+    typedef hls::stream<typename WideTaggedMacType::t_TypeInt> TaggedMacTypeStream;
 
-public:
-  static void gemm(EdgeStreamM &p_As, EdgeStreamN &p_Bs, EdgeMacStreamN &p_sum,
-                   unsigned int p_blocks) {
+   public:
+    static void gemm(EdgeStreamM& p_As, EdgeStreamN& p_Bs, EdgeMacStreamN& p_sum, unsigned int p_blocks) {
 #ifndef __SYNTHESIS__
-    assert(t_KBufferDim >= t_ParEntriesM + t_ParEntriesN);
+        assert(t_KBufferDim >= t_ParEntriesM + t_ParEntriesN);
 #endif
 #pragma HLS DATAFLOW
-    SystolicArray<t_DataType, t_KBufferDim, t_ParEntriesM, t_ParEntriesN,
-                  t_MacDataType>
-        l_sysArr(p_blocks);
-    l_sysArr.process(p_As, p_Bs, p_sum);
-  }
+        SystolicArray<t_DataType, t_KBufferDim, t_ParEntriesM, t_ParEntriesN, t_MacDataType> l_sysArr(p_blocks);
+        l_sysArr.process(p_As, p_Bs, p_sum);
+    }
 };
 
 /**
@@ -185,54 +174,53 @@ public:
  * @param p_B the input stream of packed Matrix entries
  * @param p_C the output matrix
  */
-template <typename t_DataType, unsigned int t_KBufferDim,
-          unsigned int t_ParEntries>
-void gemm(
-    const unsigned int p_m, const unsigned int p_n, const unsigned int p_k,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_A,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_B,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_C) {
+template <typename t_DataType, unsigned int t_KBufferDim, unsigned int t_ParEntries>
+void gemm(const unsigned int p_m,
+          const unsigned int p_n,
+          const unsigned int p_k,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_A,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_B,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_C) {
 #pragma HLS DATAFLOW
 #ifndef __SYNTHESIS__
-  assert(p_m % t_ParEntries == 0);
-  assert(p_n % t_ParEntries == 0);
-  assert(p_k == t_KBufferDim);
+    assert(p_m % t_ParEntries == 0);
+    assert(p_n % t_ParEntries == 0);
+    assert(p_k == t_KBufferDim);
 #endif
-  unsigned int l_blocks = p_m * p_n / t_ParEntries / t_ParEntries;
-  Gemm<t_DataType, t_KBufferDim, t_ParEntries>::gemm(p_A, p_B, p_C, l_blocks);
+    unsigned int l_blocks = p_m * p_n / t_ParEntries / t_ParEntries;
+    Gemm<t_DataType, t_KBufferDim, t_ParEntries>::gemm(p_A, p_B, p_C, l_blocks);
 }
 
-template <typename t_DataType, unsigned int t_KBufferDim,
-          unsigned int t_ParEntries, unsigned int t_MaxSizeC,
+template <typename t_DataType,
+          unsigned int t_KBufferDim,
+          unsigned int t_ParEntries,
+          unsigned int t_MaxSizeC,
           typename t_IndexType = unsigned int>
-void gemm(
-    const unsigned int p_m, const unsigned int p_n, const unsigned int p_k,
-    const t_DataType p_alpha,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_A,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_B,
-    const t_DataType p_beta,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_C,
-    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> &p_R) {
+void gemm(const unsigned int p_m,
+          const unsigned int p_n,
+          const unsigned int p_k,
+          const t_DataType p_alpha,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_A,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_B,
+          const t_DataType p_beta,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_C,
+          hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt>& p_R) {
 #ifndef __SYNTHESIS__
-  assert(p_m % t_ParEntries == 0);
-  assert(p_n % t_ParEntries == 0);
-  assert(p_k == t_KBufferDim);
+    assert(p_m % t_ParEntries == 0);
+    assert(p_n % t_ParEntries == 0);
+    assert(p_k == t_KBufferDim);
 #endif
-  unsigned int l_size = p_m * p_n;
-  unsigned int l_blocks = l_size / t_ParEntries / t_ParEntries;
+    unsigned int l_size = p_m * p_n;
+    unsigned int l_blocks = l_size / t_ParEntries / t_ParEntries;
 
-  hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> l_sum,
-      l_mat, l_betaC;
+    hls::stream<typename WideType<t_DataType, t_ParEntries>::t_TypeInt> l_sum, l_mat, l_betaC;
 
 #pragma HLS DATAFLOW
 
-  Gemm<t_DataType, t_KBufferDim, t_ParEntries, t_ParEntries>::gemm(
-      p_A, p_B, l_sum, l_blocks);
-  gemmBufferC<t_DataType, t_ParEntries, t_ParEntries, t_MaxSizeC>(p_m, p_n,
-                                                                  l_sum, l_mat);
-  scal<t_DataType, t_ParEntries, t_IndexType>(l_size, p_beta, p_C, l_betaC);
-  axpy<t_DataType, t_ParEntries, t_IndexType>(l_size, p_alpha, l_mat, l_betaC,
-                                              p_R);
+    Gemm<t_DataType, t_KBufferDim, t_ParEntries, t_ParEntries>::gemm(p_A, p_B, l_sum, l_blocks);
+    gemmBufferC<t_DataType, t_ParEntries, t_ParEntries, t_MaxSizeC>(p_m, p_n, l_sum, l_mat);
+    scal<t_DataType, t_ParEntries, t_IndexType>(l_size, p_beta, p_C, l_betaC);
+    axpy<t_DataType, t_ParEntries, t_IndexType>(l_size, p_alpha, l_mat, l_betaC, p_R);
 }
 
 } // end namespace blas
--- blas/L1/include/hw/xf_blas/gemm/transpose.hpp
+++ blas/L1/include/hw/xf_blas/gemm/transpose.hpp
@@ -19,97 +19,97 @@
 namespace xf {
 namespace blas {
 
-template <typename t_DataType, unsigned int t_ColMemWords,
+template <typename t_DataType,
+          unsigned int t_ColMemWords,
           unsigned int t_ParEntriesM,
           unsigned int t_ParEntriesN = t_ParEntriesM>
 class Transpose {
-  typedef WideType<t_DataType, t_ParEntriesM> t_WideTypeM;
-  typedef hls::stream<typename t_WideTypeM::t_TypeInt> DdrStreamM;
+    typedef WideType<t_DataType, t_ParEntriesM> t_WideTypeM;
+    typedef hls::stream<typename t_WideTypeM::t_TypeInt> DdrStreamM;
 
-  typedef WideType<t_DataType, t_ParEntriesN> t_WideTypeN;
-  typedef hls::stream<typename t_WideTypeN::t_TypeInt> DdrStreamN;
+    typedef WideType<t_DataType, t_ParEntriesN> t_WideTypeN;
+    typedef hls::stream<typename t_WideTypeN::t_TypeInt> DdrStreamN;
 
-  static const int t_BufferSize = t_ParEntriesM * t_ColMemWords;
+    static const int t_BufferSize = t_ParEntriesM * t_ColMemWords;
 
-  unsigned int m_iterationNum;
-  unsigned int m_reuseNum;
+    unsigned int m_iterationNum;
+    unsigned int m_reuseNum;
 
-public:
-  Transpose(unsigned int p_iterationNum, unsigned int p_reuseNum) {
-    m_iterationNum = p_iterationNum;
-    m_reuseNum = p_reuseNum;
-  }
+   public:
+    Transpose(unsigned int p_iterationNum, unsigned int p_reuseNum) {
+        m_iterationNum = p_iterationNum;
+        m_reuseNum = p_reuseNum;
+    }
 
-  void process(DdrStreamN &p_streamIn, DdrStreamM &p_streamOut) {
-    DdrStreamN p_s0_0, p_s0_1;
-    DdrStreamM p_s1_0, p_s1_1;
-    unsigned int l_iter1 = m_iterationNum >> 1;
-    unsigned int l_iter0 = m_iterationNum - l_iter1;
+    void process(DdrStreamN& p_streamIn, DdrStreamM& p_streamOut) {
+        DdrStreamN p_s0_0, p_s0_1;
+        DdrStreamM p_s1_0, p_s1_1;
+        unsigned int l_iter1 = m_iterationNum >> 1;
+        unsigned int l_iter0 = m_iterationNum - l_iter1;
 #pragma HLS DATAFLOW
-    split(p_streamIn, p_s0_0, p_s0_1);
-    buffer(l_iter0, p_s0_0, p_s1_0);
-    buffer(l_iter1, p_s0_1, p_s1_1);
-    merge(p_s1_0, p_s1_1, p_streamOut);
-  }
+        split(p_streamIn, p_s0_0, p_s0_1);
+        buffer(l_iter0, p_s0_0, p_s1_0);
+        buffer(l_iter1, p_s0_1, p_s1_1);
+        merge(p_s1_0, p_s1_1, p_streamOut);
+    }
 
-  void buffer(unsigned int p_iterationNum, DdrStreamN &p_in,
-              DdrStreamM &p_out) {
-    t_WideTypeN l_buffer[t_ParEntriesM][t_ColMemWords];
+    void buffer(unsigned int p_iterationNum, DdrStreamN& p_in, DdrStreamM& p_out) {
+        t_WideTypeN l_buffer[t_ParEntriesM][t_ColMemWords];
 #pragma HLS ARRAY_PARTITION variable = l_buffer dim = 1 complete
-    for (int l_block = 0; l_block < p_iterationNum; ++l_block) {
-      // read block
-      for (int i = 0; i < t_ParEntriesM; ++i) {
-        for (int j = 0; j < t_ColMemWords; j++) {
+        for (int l_block = 0; l_block < p_iterationNum; ++l_block) {
+            // read block
+            for (int i = 0; i < t_ParEntriesM; ++i) {
+                for (int j = 0; j < t_ColMemWords; j++) {
 #pragma HLS PIPELINE style = stp
-          t_WideTypeN l_word = p_in.read();
-          l_buffer[i][j] = l_word;
-        }
-      }
-      // stream down l_buffer
-      for (int r = 0; r < m_reuseNum; ++r) {
-        for (int i = 0; i < t_ColMemWords; i++) {
-          for (int j = 0; j < t_ParEntriesN; j++) {
-#pragma HLS PIPELINE style = stp
-            t_WideTypeM l_word;
-            for (int k = 0; k < t_ParEntriesM; k++) {
-              l_word[k] = l_buffer[k][i][j];
+                    t_WideTypeN l_word = p_in.read();
+                    l_buffer[i][j] = l_word;
+                }
             }
-            p_out.write(l_word);
-          }
-        }
-      }
-    }
-  }
-  void split(DdrStreamN &p_in, DdrStreamN &p_out1, DdrStreamN &p_out2) {
-    for (int i = 0; i < m_iterationNum; ++i) {
-      for (int j = 0; j < t_BufferSize; ++j) {
+            // stream down l_buffer
+            for (int r = 0; r < m_reuseNum; ++r) {
+                for (int i = 0; i < t_ColMemWords; i++) {
+                    for (int j = 0; j < t_ParEntriesN; j++) {
 #pragma HLS PIPELINE style = stp
-        t_WideTypeN l_word = p_in.read();
-        if ((i % 2) == 0) {
-          p_out1.write(l_word);
-        } else {
-          p_out2.write(l_word);
+                        t_WideTypeM l_word;
+                        for (int k = 0; k < t_ParEntriesM; k++) {
+                            l_word[k] = l_buffer[k][i][j];
+                        }
+                        p_out.write(l_word);
+                    }
+                }
+            }
         }
-      }
     }
-  }
+    void split(DdrStreamN& p_in, DdrStreamN& p_out1, DdrStreamN& p_out2) {
+        for (int i = 0; i < m_iterationNum; ++i) {
+            for (int j = 0; j < t_BufferSize; ++j) {
+#pragma HLS PIPELINE style = stp
+                t_WideTypeN l_word = p_in.read();
+                if ((i % 2) == 0) {
+                    p_out1.write(l_word);
+                } else {
+                    p_out2.write(l_word);
+                }
+            }
+        }
+    }
 
-  void merge(DdrStreamM &p_in1, DdrStreamM &p_in2, DdrStreamM &p_out) {
-    for (int i = 0; i < m_iterationNum; ++i) {
-      for (int r = 0; r < m_reuseNum; ++r) {
-        for (int j = 0; j < t_BufferSize; ++j) {
+    void merge(DdrStreamM& p_in1, DdrStreamM& p_in2, DdrStreamM& p_out) {
+        for (int i = 0; i < m_iterationNum; ++i) {
+            for (int r = 0; r < m_reuseNum; ++r) {
+                for (int j = 0; j < t_BufferSize; ++j) {
 #pragma HLS PIPELINE style = stp
-          t_WideTypeM l_word;
-          if ((i % 2) == 0) {
-            l_word = p_in1.read();
-          } else {
-            l_word = p_in2.read();
-          }
-          p_out.write(l_word);
+                    t_WideTypeM l_word;
+                    if ((i % 2) == 0) {
+                        l_word = p_in1.read();
+                    } else {
+                        l_word = p_in2.read();
+                    }
+                    p_out.write(l_word);
+                }
+            }
         }
-      }
     }
-  }
 };
 }
 }
